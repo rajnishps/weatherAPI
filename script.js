ec4d0b716461962ad80301e69647d5cad13090f7
Rajnish Pratap Singh <rps.rajnish@gmail.com>
2022-08-31 10:44:47 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const demoDiv = document.querySelector(".demo");
+const placeInput = document.querySelector(".placeInput");
+const searchBtn = document.querySelector(".searchBtn");
+
+const apiKey = "5d193aa4e224e170b6490576cf40f0dc";
+let place = null;
+
+placeInput.addEventListener("keyup", (event) => {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    searchBtn.click();
+  }
+});
+
+searchBtn.onclick = () => {
+  place = placeInput.value;
+  longlat(place);
+};
+
+const longlat = async (place) => {
+  const baseURL = await fetch(
+    `https://api.openweathermap.org/geo/1.0/direct?q=${place}&limit=1&appid=${apiKey}`
+  );
+  const data = await baseURL.json();
+  weather(data[0].lat, data[0].lon);
+};
+
+const weather = async (lat, lon) => {
+  const baseURL = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=en&appid=${apiKey}&units=metric`
+  );
+  const data = await baseURL.json();
+  current(data);
+};
+
+const current = (data) => {
+  console.log(data);
+  const name = `<h2>${data.name}, ${place}, ${data.sys.country}</h2>`;
+  const currWeather = `<h2>${data.weather[0].main}</h2>`;
+  const humidity = `<h2>Humidity: ${data.main.humidity}</h2>`;
+  const temp = `<h2>Temp: ${data.main.temp}</h2>`;
+  const feelstemp = `<h2>Feels Like: ${data.main.feels_like}</h2>`;
+
+  demoDiv.innerHTML = `${name}${currWeather}${humidity}${temp}${feelstemp}`;
+};
